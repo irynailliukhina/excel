ba99917fddba51912cece16d78cc1d287b082e82
iryna.illiukhina iryna.illiukhina@vaimo.com <iryna.illiukhina@vaimo.com>
2021-11-04 19:22:14 +0100

    finish table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -2,13 +2,29 @@
 import {DomListener} from "./DomListener";
 
 export class ExcelComponent extends DomListener{
-    constructor($root, options={}) {
+    constructor($root, options = {}) {
         super($root, options.listeners);
         this.name = options.name || ''
+        this.emitter = options.emitter
+        this.unsubscribers = []
+        this.prepare()
+    }
+
+    prepare(){
+
     }
 
     toHtml(){
         return ''
+    }
+
+    $emit(event, ...args){
+        this.emitter.emit(event, ...args)
+    }
+
+    $on(event, fn){
+        const unsub = this.emitter.subscribe(event, fn)
+        this.unsubscribers.push(unsub)
     }
 
     init(){
@@ -17,5 +33,6 @@ export class ExcelComponent extends DomListener{
 
     destroy(){
         this.removeDomListeners()
+        this.unsubscribers.forEach(unsub => unsub())
     }
 }
