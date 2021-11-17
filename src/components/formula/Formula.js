af4bf88ab4a4cd709f67787f60ad192d5f2cabcc
iryna.illiukhina iryna.illiukhina@vaimo.com <iryna.illiukhina@vaimo.com>
2021-11-17 11:45:53 +0100

    Finish redux and table logic

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -10,6 +10,7 @@ export class Formula extends ExcelComponent{
         super($root, {
             name: 'Formula',
             listeners: ['input', 'keydown'],
+            subscribe: ['currentText'],
             ...options
         });
         this.selection = new TableSelection()
@@ -22,11 +23,7 @@ export class Formula extends ExcelComponent{
         this.$formula = this.$root.find('#formula')
 
         this.$on('table:select', $cell =>{
-            this.$formula.text($cell.text())
-        })
-
-        this.$on('table:input', $cell => {
-            this.$formula.text($cell.text())
+            this.$formula.text($cell.data.value)
         })
     }
 
@@ -35,6 +32,10 @@ export class Formula extends ExcelComponent{
         <div class="info">fx</div>
       <div id = "formula" class="input" contenteditable spellcheck="false"></div>
         `
+    }
+
+    storeChanged({currentText}) {
+        this.$formula.text(currentText)
     }
 
     onInput(event){
